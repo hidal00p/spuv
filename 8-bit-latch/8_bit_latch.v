c9354b19e8c05943aa76cb7e208b812be68e6905
Anton Kovalov <anton.kov.hack@proton.me>
2024-11-06 17:50:05 +0100

    refactor[mem]: clk -> write signal

--- 8-bit-latch/8_bit_latch.v
+++ 8-bit-latch/8_bit_latch.v
@@ -1,10 +1,10 @@
 `include "dff.v"
 
-module eight_bit_latch(input clk, input [7:0] data, output [7:0] q);
+module eight_bit_latch(input write, input [7:0] data, output [7:0] q);
     genvar i;
     generate
         for (i = 0; i <= 7; i = i + 1) begin : full_latch
-            dff one_bit_latch(clk, data[i], q[i]);
+            dff one_bit_latch(write, data[i], q[i]);
         end
     endgenerate
 endmodule
--- 8-bit-latch/8_bit_latch_tb.v
+++ 8-bit-latch/8_bit_latch_tb.v
@@ -2,28 +2,28 @@
 `timescale 1ns / 1ns
 
 module eight_bit_latch_tb;
-    reg clk;
+    reg write;
     reg [7:0] data;
     wire [7:0] q;
 
-    eight_bit_latch uut(clk, data, q);
+    eight_bit_latch uut(write, data, q);
 
     initial begin
-        clk = 1; data = 8'b00000000;
+        write = 1; data = 8'b00000000;
         #10
-        $display("%b %b %b", clk, data, q);
+        $display("%b %b %b", write, data, q);
 
-        clk = 0; data = 8'b00000101;
+        write = 0; data = 8'b00000101;
         #10
-        $display("%b %b %b", clk, data, q);
+        $display("%b %b %b", write, data, q);
 
-        clk = 1;
+        write = 1;
         #10
-        $display("%b %b %b", clk, data, q);
+        $display("%b %b %b", write, data, q);
 
-        clk = 0; data = 8'b10000000;
+        write = 0; data = 8'b10000000;
         #10
-        $display("%b %b %b", clk, data, q);
+        $display("%b %b %b", write, data, q);
 
         $display("Test Completed");
     end
